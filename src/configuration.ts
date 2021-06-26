a66121264e417ab01b70d951f338ec524aa3a71b
xiaojunnuo <xiaojunnuo@qq.com>
2021-06-26 15:19:29 +0800

    1.1.6

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,4 +1,4 @@
-import { Config, Configuration, Logger } from '@midwayjs/decorator';
+import { ALL, Config, Configuration, Logger } from '@midwayjs/decorator';
 import { Flyway } from './flyway';
 import { ILogger } from '@midwayjs/logger';
 
@@ -7,13 +7,13 @@ import { ILogger } from '@midwayjs/logger';
   //importConfigs: [join(__dirname, './config')],
 })
 export class FlywayConfiguration {
-  @Config('flyway')
-  flyway;
+  @Config(ALL)
+  config;
 
   @Logger()
   logger: ILogger;
   async onReady() {
-    this.logger.info('flyway start:', this.flyway);
-    await new Flyway({ ...this.flyway, logger: this.logger }).run();
+    this.logger.info('flyway start:', this.config.flyway);
+    await new Flyway({ ...this.config.flyway, logger: this.logger }).run();
   }
 }
